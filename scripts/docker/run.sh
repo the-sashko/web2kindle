6137480f88a20d03560a647a79ce077270c28d9b
The Sashko <18634500+the-sashko@users.noreply.github.com>
2022-05-31 20:51:59 +0300

    Update run.sh
    improve docker run script

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -9,9 +9,10 @@ sudo docker pull iamsashko/web2kindle
 
 sudo docker rm web2kindle
 
-sudo docker run -d -p 80:80 --name web2kindle -v tmp:/storage/web2kindle/tmp \
-  -v config:/storage/web2kindle/config \
-  -v logs:/storage/web2kindle/logs \
+sudo docker run -d -p 80:80 --name web2kindle \
+  -v "$scriptDir/../../tmp":/storage/web2kindle/tmp \
+  -v "$scriptDir/../../config":/storage/web2kindle/config \
+  -v "$scriptDir/../../logs":/storage/web2kindle/logs \
   iamsashko/web2kindle
 
 cd "$currentDir" || exit 1
